23e20bef5a148a4aff4c1d3ad3b5f8d13b2c27da
Yuntae <nralpaca@gmail.com>
2021-01-24 00:12:12 +0900

    init

---
+++ simple-board/src/test/java/com/yntkim/simpleboard/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yntkim.simpleboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
